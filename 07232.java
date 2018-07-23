f6320fafd632c01ed576fcb2810b8fc09d38377e
rakun1228 <38870433+rakun1228@users.noreply.github.com>
2018-07-23 15:17:51 +0900

    Create 07232.java
    180723
    input registration numver, return gender

---
+++ 07232.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class  test07231
+{
+	public static String strInput()
+	{
+		Scanner scan=new Scanner(System.in);  //문자열 입력
+		String s;
+
+		s=scan.nextLine();
+		return s;
+	}
+  
+	public static String reverse(String str)  //문자열을 거꾸로
+	{
+		String rel;
+		
+		rel="";    //문자열 초기화
+
+		for(int i=0;i<str.length();i++)
+		{
+			rel+=str.charAt(str.length()-i-1);
+		}
+		return rel;
+
+	}
+	
+	public static boolean gender(String str) //주민번호 남녀구분
+	{
+		if (str.charAt(7)=='1'||str.charAt(7)=='3')   //not ==1  
+			return true;
+		return false;
+	}
+	
+	public static void main(String[] args) 
+	{
+		boolean b;
+
+		b=gender(strInput());
+
+		if(b==true)
+			System.out.println("남자");
+		else
+			System.out.println("여자");
+	}
+}

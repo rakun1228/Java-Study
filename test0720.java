4f3abe4bcf2faa15905281c5dbbb65caf823ce85
rakun1228 <38870433+rakun1228@users.noreply.github.com>
2018-07-23 14:30:28 +0900

    Create test0720.java
    180720
    local variable&global variable

---
+++ test0720.java
@@ -0,0 +1,15 @@
+class test07201 
+{
+	public static void func() 
+	{
+		int a=0;
+		a+=3;
+		System.out.println(a);
+	}
+	public static void main(String[] args) 
+	{
+		func();
+		func();
+		func();
+	}
+}
